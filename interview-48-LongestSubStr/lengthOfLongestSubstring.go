1fbcec9dd9532b6a5f6106097597a4fc696031f2
芦文超 <bighandsome@luwenchao-2.local>
2020-10-12 09:32:15 +0800

    modify

---
+++ interview-48-LongestSubStr/lengthOfLongestSubstring.go
@@ -0,0 +1,49 @@
+package interview_48_LongestSubStr
+
+import "strings"
+
+/*
+请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+
+ 
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+*/
+func LengthOfLongestSubstring(s string) int {
+	if len(s) <= 1 {
+		return len(s)
+	}
+	res, before := 0, 0
+	slice, deDup := strings.Split(s, ""), make(map[string]int, 0)
+	for i := 0; i < len(slice); i++ {
+		if index, ok := deDup[slice[i]]; ok {
+			before = index + 1
+			for k, v := range deDup {
+				if v <= index {
+					delete(deDup, k)
+				}
+			}
+		} else {
+			if i-before > res {
+				res = i - before
+			}
+		}
+		deDup[slice[i]] = i
+	}
+	return res + 1
+}
---
+++ interview-48-LongestSubStr/lengthOfLongestSubstring_test.go
@@ -0,0 +1,10 @@
+package interview_48_LongestSubStr
+
+import (
+	"log"
+	"testing"
+)
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	log.Println(LengthOfLongestSubstring("pwwkew"))
+}
